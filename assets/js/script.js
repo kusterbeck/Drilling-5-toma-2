568651d006a061564cdf67abc739a58f6fbbe0b4
doug <dmorgadomiranda@gmail.com>
2023-06-14 01:06:42 -0400

    card funcionando sin un ciclo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,39 @@
 let arrayCharacters = [];
+let isFetchComplete = false;
 let pageIndex = 0;
 let punteroColores = 0;
 let punteroTexto = 0;
 let punteroHideCard = 0;
+let ingresarHtml = document.getElementById('main_box');
  
-
-
 async function fetchData() {
     for (let i = 0; i < 9; i++) {
-        let url = `https://swapi.dev/api/people/?page=${i+1}`;
+        let url = `https://swapi.dev/api/people/?page=${i+1}&format=json`;
         try {
             let response = await fetch(url);
             response = await response.json();
             arrayCharacters.push(response.results);
             // console.log(arrayCharacters);
+            
         } catch (error) {
             console.log(error);
         }
         console.log(arrayCharacters)
+        isFetchComplete = true;
     }
+    generar.next().value;
 }
 
 async function renderData() {
-    if (arrayCharacters == []) {
-        await fetchData();
-    }
-    
-    let text = ['Encontrarás información sobre los personajes más populares de las películas.',
-                'Encontrarás información sobre los personajes secundarios importantes.', 
-                'Encontrarás información sobre otros personajes significativos']
-    let colores = ['rojo', 'verde', 'celeste'];
-    let hideCard = ['','hidden'];
+    if (isFetchComplete === true) {
+        let text = ['Encontrarás información sobre los personajes más populares de las películas.',
+                    'Encontrarás información sobre los personajes secundarios importantes.', 
+                    'Encontrarás información sobre otros personajes significativos']
+        let colores = ['rojo', 'verde', 'celeste'];
+        let hideCard = ['','hidden'];
+        let createCard = document.createElement('div');
+        createCard.classList.add('row');
+        createCard.setAttribute('id', `row_${pageIndex}`);
         let cards = `<div class="card mb-3 ${hideCard[punteroHideCard]}" style="max-width: 340px;" id="bloque_${pageIndex}" onclick="generar.next().value;">
                         <div class="row g-0">
                             <div class="col-md-2">
@@ -54,57 +57,38 @@ async function renderData() {
         if (punteroTexto >= 2) {
             punteroTexto = 2;
         }
-        let ingresarHtml = document.getElementById('main_box');
-        ingresarHtml.innerHTML += cards;
+        console.log(ingresarHtml);
+        ingresarHtml.appendChild(createCard);
+        createCard.innerHTML = cards;
+        renderCards();
+    }
 }
 
 function renderCards() {
-    for (let i = 0; i < 1; i++) {
-        for (let j = 0; j <= 9; j++) {
-        let characterCard = `<div class="card mb-3 card_height" style="max-width: 340px;">
-                                <div class="row g-0">
-                                    <div class="col-md-2">
-                                        <div class="circulo verde"></div>
-                                    </div>
-                                    <div class="col-md-10">
-                                        <div class="card-body">
-                                            <h5 class="card-title">${arrayCharacters[i][j].name}</h5>
-                                            <p class="card-text">Estatura: ${arrayCharacters[i][j].height} cm. Peso: ${arrayCharacters[i][j].mass} kg.</p>
-                                        </div>
+        const attachCard = document.getElementById(`row_${pageIndex-1}`);
+        let card = document.createElement('div')
+        card.classList.add('card', 'mb-3', 'card_height');  
+        card.innerHTML = `<div class="row g-0">
+                                <div class="col-md-2">
+                                    <div class="circulo verde"></div>
+                                </div>
+                                <div class="col-md-10">
+                                    <div class="card-body">
+                                        <h5 class="card-title">titulo</h5>
+                                        <p class="card-text">Estatura: 150 cm. Peso: 45 kg.</p>
                                     </div>
                                 </div>
                             </div>
-                            `;
-        html += characterCard;
-        }
-    }
-    let container = document.getElementById(`row_${n}`);
-    container.innerHTML = html;
+                        `;
+        attachCard.appendChild(card);
+        // let container = document.getElementById(`row_${pageIndex-1}`);
+
 }
-
-// function carta1(clase, parrafo) {
-//     let html1 = `<div class="card mb-3 " style="max-width: 340px;" id="bloque_1" onclick="generar.next().value;">
-//                     <div class="row g-0">
-//                         <div class="col-md-2">
-//                             <div class="circulo ${clase}"></div>
-//                         </div>
-//                         <div class="col-md-10">
-//                             <div class="card-body">
-//                                 <h5 class="card-title">En esta sección...</h5>
-//                                 <p class="card-text">${parrafo}</p>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>`;
-//     let ingresarHtml = document.getElementById('main_box');
-//     ingresarHtml.innerHTML += html1;
-// }
-
-const generar = cardGenerator();
-generar.next().value;
 
 
 fetchData();
+const generar = cardGenerator();
+
 function* cardGenerator() {
     
     console.log(0)
